33cca3c32c77def4b89550ffef2631f4fbb5a834
Larry Buchanan <larrybuch@gmail.com>
2012-11-26 18:26:32 -0500

    added users, logins, welcome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def login_or_logout
     if @authenticated_user
-      link_to "log out", logout_path
+      link_to "log out #{@authenticated_user.name}", logout_path
     else
       link_to 'log in', login_path
     end
