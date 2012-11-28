1d2beeac715ebf21cd4378b3a6877a89aecf5664
Zahra Jabini <zahra.arch@gmail.com>
2012-11-27 23:59:55 -0500

    javascript for checkboxes figured out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,28 @@
 //= require jquery
 //= require jquery_ujs
 //= require 'js/bootstrap.js'
+//= require_self
+
+$(function(){
+	// $("input[name='bought_item_id']").click(function(){
+	// 	$("input[name='bought_item_id']").attr("checked", false); 
+	// 	$(this).attr("checked", true);
+	// });
+
+	// $("input[name='sold_item_id']").click(function(){
+	// 	$("input[name='sold_item_id']").attr("checked", false); 
+	// 	$(this).attr("checked", true);
+	// });
+
+	// whebever a checkbox is checked
+	// find all other checkboxes that have the same name
+	// as the checkbox that was checked, and uncheck em
+	// then check that one in
+
+	$("input:checkbox").click(function(){
+		name_of_checkboxes = $(this).attr("name");
+		$("input[name='"+ name_of_checkboxes + "']").attr("checked", false); 
+		$(this).attr("checked", true);
+	});
+
+});
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -15,3 +15,20 @@ class Trade < ActiveRecord::Base
 	belongs_to :buyer, :class_name => 'User', :foreign_key => 'buyer_id'
 
 end
+
+
+# items
+# 1 			"Laptop"			1
+# 2			"Chair"				2
+
+
+# users
+# 1		"QA"
+# 2		"Z"
+
+# trades
+# sold_item_id   seller_id buyer_id bought_item_id
+# 1				1       2              2
+
+# When creating a trade, we want to change the ownership of the items
+# and create a record of that trade with who sold what to whom.
